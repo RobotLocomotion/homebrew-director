0a720c6cbcbff0e9845dace27ee4670dec116058
Jamie Snape <jamie.snape@kitware.com>
2018-01-10 16:03:02 -0500

    Add bazel@0.8 formula

---
+++ Formula/bazel@0.8.rb
@@ -0,0 +1,106 @@
+# Copyright 2012-2018 Robot Locomotion Group @ CSAIL. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#  3. Neither the name of the copyright holder nor the names of its
+#     contributors may be used to endorse or promote products derived from
+#     this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Copyright 2009-2018 Homebrew contributors.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+class BazelAT08 < Formula
+  desc "Google's own build tool"
+  homepage "https://bazel.build/"
+  url "https://drake-homebrew.csail.mit.edu/mirror/bazel-0.8.1.tar.gz"
+  sha256 "8e2fd993642b7965beb615d7039ec8364376b52d5dd5d11bfee002060a3ce15e"
+  head "https://github.com/bazelbuild/bazel.git"
+
+  bottle do
+    cellar :any_skip_relocation
+    root_url "https://drake-homebrew.csail.mit.edu/bottles"
+  end
+
+  keg_only :versioned_formula
+
+  depends_on :java => "1.8"
+  depends_on :macos => :yosemite
+
+  def install
+    ENV["EMBED_LABEL"] = "#{version}-homebrew"
+    ENV["BAZEL_WRKDIR"] = buildpath/"work"
+
+    system "./compile.sh"
+    system "./output/bazel", "--output_user_root", buildpath/"output_user_root",
+           "build", "scripts:bash_completion"
+
+    bin.install "scripts/packages/bazel.sh" => "bazel"
+    bin.install "output/bazel" => "bazel-real"
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
+
+    bash_completion.install "bazel-bin/scripts/bazel-complete.bash"
+    zsh_completion.install "scripts/zsh_completion/_bazel"
+  end
+
+  test do
+    touch testpath/"WORKSPACE"
+
+    (testpath/"ProjectRunner.java").write <<~EOS
+      public class ProjectRunner {
+        public static void main(String args[]) {
+          System.out.println("Hi!");
+        }
+      }
+    EOS
+
+    (testpath/"BUILD").write <<~EOS
+      java_binary(
+        name = "bazel-test",
+        srcs = glob(["*.java"]),
+        main_class = "ProjectRunner",
+      )
+    EOS
+
+    system bin/"bazel", "build", "//:bazel-test"
+    system "bazel-bin/bazel-test"
+  end
+end
