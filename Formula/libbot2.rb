1cbd92d911155d83573ba5f6ed2d73e6ec427e11
Jamie Snape <jamie.snape@kitware.com>
2018-03-13 21:30:51 -0400

    Update libbot2 bottles

--- Formula/libbot2.rb
+++ Formula/libbot2.rb
@@ -34,9 +34,9 @@ class Libbot2 < Formula
 
   bottle do
     root_url "https://drake-homebrew.csail.mit.edu/bottles"
-    sha256 "3fdaf804877a0581713f3fad82316c42c9f3ce7a711b889d2d33f314c199fc31" => :high_sierra
-    sha256 "a9f857f97d257cc8fce94d3b8da2cadf0539ba7fdb76f2636d8510a464ee4ba7" => :sierra
-    sha256 "c4200c669fc1f4f1e5fd8fbeed3758f031ae3a2f8bfa08823b488aeb0380091e" => :el_capitan
+    sha256 "56d041ebc517630306ad07018d710bc8c99fcc42c9cd64db66a0a9c6cd8dbe40" => :high_sierra
+    sha256 "cfa1e9f92039b7fa5aa5dfcf22870152252d8f685d05acb607b6ec299343699b" => :sierra
+    sha256 "8da0ab61fa468802b417a840abdabc9c67d403a51a3f0c135a2b66ed6b1780eb" => :el_capitan
   end
 
   depends_on :java
