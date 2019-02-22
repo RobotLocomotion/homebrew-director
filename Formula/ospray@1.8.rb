b5404c01cfa751e29897d98483c85d9a90a71ce8
Jamie Snape <jamie.snape@kitware.com>
2019-02-22 11:15:23 -0500

    Add ospray@1.8 formula

---
+++ Formula/ospray@1.8.rb
@@ -0,0 +1,81 @@
+# Copyright (c) 2019, Massachusetts Institute of Technology.
+# Copyright (c) 2019, Toyota Research Institute.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+class OsprayAT18 < Formula
+  desc "Ray-tracing-based rendering engine for high-fidelity visualization"
+  homepage "https://www.ospray.org/"
+  url "https://drake-homebrew.csail.mit.edu/mirror/ospray-1.8.0.tar.gz"
+  sha256 "45b094c8ff8be42ab694d65e9a2f6c102c9228e67cf33109ed73a8f1621256b8"
+  head "https://github.com/ospray/ospray.git"
+
+  bottle do
+    cellar :any
+    root_url "https://drake-homebrew.csail.mit.edu/bottles"
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "cmake" => :build
+  depends_on "ispc" => :build
+  depends_on "embree@3.5"
+  depends_on "tbb"
+
+  def install
+    ENV["HOMEBREW_OPTFLAGS"] = ""
+
+    args = std_cmake_args + %W[
+      -Dembree_DIR=#{Formula["embree@3.5"].opt_lib}/cmake/embree-3.5.0"
+      -DOSPRAY_ENABLE_APPS=OFF
+      -DOSPRAY_ENABLE_TESTING=OFF
+      -DOSPRAY_ENABLE_TUTORIALS=OFF
+    ]
+
+    mkdir "build" do
+      system "cmake", *args, ".."
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"version.cpp").write <<~EOS
+      #include <cassert>
+      #include <ospray/version.h>
+      int main() {
+        assert(OSPRAY_VERSION_MAJOR == 1);
+        assert(OSPRAY_VERSION_MINOR == 8);
+        assert(OSPRAY_VERSION_PATCH == 0);
+        return 0;
+      }
+    EOS
+
+    system ENV.cxx, "-std=c++11", "version.cpp", "-I#{opt_include}"
+    system "./a.out"
+  end
+end
