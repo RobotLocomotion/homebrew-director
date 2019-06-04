c803a240e20227f028e2956d0611e6dc238f7b6a
Jamie Snape <jamie.snape@kitware.com>
2019-06-04 10:12:02 -0400

    Add ipopt@3.12 formula

---
+++ Formula/ipopt@3.12.rb
@@ -0,0 +1,107 @@
+# Copyright (c) 2019, Massachusetts Institute of Technology.
+# Copyright (c) 2019, Toyota Research Institute.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Copyright (c) 2009-2018, Homebrew contributors.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+class IpoptAT312 < Formula
+  desc "Interior point optimizer"
+  homepage "https://coin-or.github.io/Ipopt/"
+  url "https://drake-homebrew.csail.mit.edu/mirror/ipopt-3.12.13.tar.gz"
+  sha256 "1ef1d43f3a6eafdb404d7630b644626d142798b296df3fddbce4898a4dcdae4f"
+
+  keg_only :versioned_formula
+
+  depends_on "mumps"
+  depends_on "openblas"
+
+  def install
+    ENV.delete("MPICC")
+    ENV.delete("MPICXX")
+    ENV.delete("MPIFC")
+
+    args = [
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--enable-shared",
+      "--prefix=#{prefix}",
+      "--with-blas=-L#{Formula["openblas"].opt_lib} -lopenblas",
+      "--with-mumps-incdir=#{Formula["mumps"].include}",
+      "--with-mumps-lib=-L#{Formula["mumps"].opt_lib} -ldmumps",
+    ]
+
+    system "./configure", *args
+    system "make"
+
+    ENV.deparallelize
+    system "make", "install"
+
+    inreplace "#{lib}/pkgconfig/ipopt.pc", prefix, opt_prefix
+    inreplace "#{lib}/pkgconfig/ipopt.pc", /-lipopt.*/, "-lipopt"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <cassert>
+      #include <IpIpoptApplication.hpp>
+      #include <IpReturnCodes.hpp>
+      #include <IpSmartPtr.hpp>
+      int main() {
+        Ipopt::SmartPtr<Ipopt::IpoptApplication> app = IpoptApplicationFactory();
+        Ipopt::ApplicationReturnStatus status = app->Initialize();
+        assert(status == Ipopt::Solve_Succeeded);
+        return 0;
+      }
+    EOS
+
+    system ENV.cxx, "test.cpp", "-I#{include}/coin", "-L#{lib}", "-lipopt"
+    system "./a.out"
+  end
+end
