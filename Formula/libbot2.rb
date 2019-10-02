98554fbbe4981680ebb767f687cf2e928423b90a
Jamie Snape <jamie.snape@kitware.com>
2019-10-02 12:01:03 -0400

    Update libbot2 to 0.0.1.20191002

--- Formula/libbot2.rb
+++ Formula/libbot2.rb
@@ -30,74 +30,51 @@
 class Libbot2 < Formula
   desc "Libraries, tools, and algorithms for robotics research"
   homepage "https://github.com/RobotLocomotion/libbot2/"
-  url "https://drake-homebrew.csail.mit.edu/mirror/libbot2-0.0.1.20181025.tar.gz"
-  sha256 "212fdd854c4f6ce986dcc908b276d2d30ad29a4da9f67be813d231e946a15ee2"
-  revision 1
-  head "https://github.com/RobotLocomotion/libbot2.git"
+  url "https://drake-homebrew.csail.mit.edu/mirror/libbot2-0.0.1.20191002.tar.gz"
+  sha256 "efe09672bdcc47e8fbd8320af5e04728debc512a4313caad061f8c3da5d664c1"
 
   bottle do
     root_url "https://drake-homebrew.csail.mit.edu/bottles"
-    sha256 "d26adaadd1db3dce33f72c9beb33b11c4d64fa65dcc996631666688555102bb4" => :mojave
-    sha256 "dd57cfe36ede4ae2f7c1825cadb86c4b372b36d87050e6d545aaabee7d2015f6" => :high_sierra
   end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "glib"
-  depends_on "gtk+"
-  depends_on :java
-  depends_on "jpeg"
-  depends_on "lcm@1.4"
-  depends_on "libpng"
+  depends_on :java => "1.8+"
+  depends_on "lcm"
   depends_on "numpy"
-  depends_on "pygobject"
-  depends_on "pygtk"
   depends_on "python"
-  depends_on "python@2"
   depends_on "scipy"
-  depends_on :x11
+  uses_from_macos "zlib"
 
   def install
-    # bot-log2mat and bot-procman-sheriff will use python2.
-    ["python3", "python2"].each do |python|
-      python_executable = `which #{python}`.strip
+    args = std_cmake_args + %W[
+      -DCMAKE_INSTALL_NAME_DIR=#{opt_lib}
+      -DCMAKE_INSTALL_RPATH=#{opt_lib}
+      -DPYTHON_EXECUTABLE=#{Formula["python"].opt_bin}/python3
+      -DWITH_BOT_VIS=OFF
+    ]
 
-      args = std_cmake_args + %W[
-        -DGLUT_glut_LIBRARY=/System/Library/Frameworks/GLUT.framework
-        -DPYTHON_EXECUTABLE='#{python_executable}'
-        -DWITH_BOT_VIS=OFF
-      ]
-
-      mkdir "build-#{python}" do
-        system "cmake", *args, ".."
-        system "make"
-        system "make", "install"
-      end
-
-      inreplace "#{bin}/bot-spy", prefix, opt_prefix
-      inreplace "#{lib}/pkgconfig/bot2-core.pc", prefix, opt_prefix
-      inreplace "#{lib}/pkgconfig/bot2-frames.pc", prefix, opt_prefix
-      inreplace "#{lib}/pkgconfig/bot2-lcmgl-client.pc", prefix, opt_prefix
-      inreplace "#{lib}/pkgconfig/bot2-lcmgl-renderer.pc", prefix, opt_prefix
-      inreplace "#{lib}/pkgconfig/bot2-param-client.pc", prefix, opt_prefix
-      inreplace "#{lib}/pkgconfig/lcmtypes_bot2-core.pc", prefix, opt_prefix
-      inreplace "#{lib}/pkgconfig/lcmtypes_bot2-frames.pc", prefix, opt_prefix
-      inreplace "#{lib}/pkgconfig/lcmtypes_bot2-lcmgl.pc", prefix, opt_prefix
-      inreplace "#{lib}/pkgconfig/lcmtypes_bot2-param.pc", prefix, opt_prefix
-      inreplace "#{lib}/pkgconfig/lcmtypes_bot2-procman.pc", prefix, opt_prefix
-
-      next unless python == "python2"
-
-      inreplace "#{bin}/bot-log2mat", prefix, opt_prefix
-      inreplace "#{bin}/bot-procman-sheriff", prefix, opt_prefix
-
-      python_version = "python" + `#{python_executable} -c 'import sys;print(sys.version[:3])'`.chomp
-      inreplace "#{lib}/#{python_version}/site-packages/bot_procman/build_prefix.py",
-        prefix, opt_prefix
+    mkdir "build" do
+      system "cmake", *args, ".."
+      system "make"
+      system "make", "install"
     end
+
+    inreplace bin/"bot-spy", prefix, opt_prefix
+    inreplace lib/"pkgconfig/bot2-core.pc", prefix, opt_prefix
+    inreplace lib/"pkgconfig/bot2-frames.pc", prefix, opt_prefix
+    inreplace lib/"pkgconfig/bot2-lcmgl-client.pc", prefix, opt_prefix
+    inreplace lib/"pkgconfig/bot2-lcmgl-renderer.pc", prefix, opt_prefix
+    inreplace lib/"pkgconfig/bot2-param-client.pc", prefix, opt_prefix
+    inreplace lib/"pkgconfig/lcmtypes_bot2-core.pc", prefix, opt_prefix
+    inreplace lib/"pkgconfig/lcmtypes_bot2-frames.pc", prefix, opt_prefix
+    inreplace lib/"pkgconfig/lcmtypes_bot2-lcmgl.pc", prefix, opt_prefix
+    inreplace lib/"pkgconfig/lcmtypes_bot2-param.pc", prefix, opt_prefix
+    inreplace lib/"pkgconfig/lcmtypes_bot2-procman.pc", prefix, opt_prefix
   end
 
   test do
-    system "#{bin}/bot-log2mat", "-h"
+    system bin/"bot-log2mat", "-h"
   end
 end
