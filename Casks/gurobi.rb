6e0b2f76e03410d71c4f5a20b0a731c3db45b2b7
Jamie Snape <jamie.snape@kitware.com>
2020-05-08 14:45:01 -0400

    Update gurobi cask to 9.0.2

--- Casks/gurobi.rb
+++ Casks/gurobi.rb
@@ -28,13 +28,13 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 cask 'gurobi' do
-  version '8.1.1'
-  sha256 'ef7459867ffa1f7455d3eca8ae6a621603c49b0369c3de882a730f198229d520'
+  version '9.0.2'
+  sha256 '1d58586f9a33ac7035f51f6a04707248218f70bddbec78cb83f11c986ac652cd'
 
   url "https://packages.gurobi.com/#{version.major_minor}/gurobi#{version}_mac64.pkg"
-  appcast 'https://www.gurobi.com/resources/documentation/fixes'
+  appcast 'https://www.gurobi.com/resource/fixes/'
   name 'Gurobi Optimizer'
-  homepage 'https://www.gurobi.com/products/gurobi-optimizer'
+  homepage 'https://www.gurobi.com/products/gurobi-optimizer/'
 
   conflicts_with cask: 'gurobi80'
 
@@ -42,7 +42,6 @@ cask 'gurobi' do
 
   uninstall pkgutil: "com.gurobi.gurobiOptimizer#{version.no_dots}.gurobimac.pkg",
             delete:  [
-                       "/Applications/Gurobi #{version}.app",
                        "/Library/gurobi#{version.no_dots}",
                        '/Library/Java/Extensions/gurobi.jar',
                        "/Library/Java/Extensions/libGurobiJni#{version.major_minor.no_dots}.jnilib",
@@ -52,12 +51,9 @@ cask 'gurobi' do
                        '/usr/local/bin/grbprobe',
                        '/usr/local/bin/grbtune',
                        '/usr/local/bin/gurobi_cl',
-                       '/usr/local/bin/gurobi.env',
                        '/usr/local/bin/gurobi.sh',
                        '/usr/local/lib/gurobi.py',
                        "/usr/local/lib/libgurobi#{version.major_minor.no_dots}.dylib",
-                       '/Library/Python/2.7/site-packages/gurobipy',
-                       "/Library/Python/2.7/site-packages/gurobipy-#{version}-py2.7.egg-info",
                      ]
 
   caveats do
