fb0f4f89ec4fd94d93e03dcbf71f42b62084aedf
Jamie Snape <jamie.snape@kitware.com>
2021-01-13 14:00:28 -0500

    Update drake cask to 0.25.0

--- Casks/drake.rb
+++ Casks/drake.rb
@@ -28,17 +28,17 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 cask "drake" do
-  version "0.23.0"
-  sha256 "9b1c65f0eb7f1952b4207b31e207d1ae26c1f8f068922d99ea7fa33f9e30ca25"
+  version "0.25.0"
+  sha256 "e6dbf3f223719188941e043bf29b2e93169b6709a6320ea94637a3519c728261"
 
-  # github.com/RobotLocomotion/drake was verified as official when first introduced to the cask
-  url "https://github.com/RobotLocomotion/drake/releases/download/v#{version}/drake-20200913-mac.tar.gz"
+  url "https://github.com/RobotLocomotion/drake/releases/download/v#{version}/drake-20201215-mac.tar.gz",
+    verified: "https://github.com/RobotLocomotion/drake/"
   appcast "https://github.com/RobotLocomotion/drake/releases.atom"
   name "Drake"
   desc "Model-based design and verification for robotics"
   homepage "https://drake.mit.edu/"
 
-  depends_on cask: "adoptopenjdk"
+  depends_on formula: "double-conversion"
   depends_on formula: "eigen"
   depends_on formula: "gcc"
   depends_on formula: "gflags"
@@ -50,22 +50,23 @@ cask "drake" do
   depends_on formula: "libyaml"
   depends_on formula: "lz4"
   depends_on formula: "nlopt"
-  depends_on formula: "numpy"
+  depends_on formula: "robotlocomotion/director/numpy@1.19.4"
   depends_on formula: "openblas"
   depends_on formula: "python@3.8"
-  depends_on formula: "scipy"
+  depends_on formula: "robotlocomotion/director/scipy@1.5.4"
   depends_on formula: "suite-sparse"
   depends_on formula: "tinyxml"
   depends_on formula: "tinyxml2"
   depends_on formula: "robotlocomotion/director/vtk@8.2"
+  depends_on formula: "xz"
   depends_on formula: "yaml-cpp"
   depends_on formula: "zeromq"
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :catalina"
 
   artifact "drake", target: "/opt/drake"
 
   postflight do
-    system_command "/usr/local/bin/pip3",
+    system_command "/usr/local/opt/python@3.8/bin/pip3.8",
                    args: [
                      "install",
                      "--disable-pip-version-check",
@@ -95,9 +96,15 @@ cask "drake" do
   uninstall delete: ["/opt/drake"]
 
   caveats do
+    depends_on_java "8+"
     path_environment_variable "/opt/drake/bin"
 
     <<~EOS
+      If you did not pass the --no-quarantine flag to
+      brew install --cask #{token} or brew upgrade --cask #{token} you may
+      experience issues at runtime. This is considered to be a bug in #{token},
+      which will be addressed a future release.
+
       For compilers to find #{token} you may need to set:
         export LDFLAGS="-L/opt/drake/lib -Wl,-rpath,/opt/drake/lib"
         export CPPFLAGS="-I/opt/drake/include"
